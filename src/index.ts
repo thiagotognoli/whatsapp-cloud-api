2d28953a47449ee0eaddc646dbed3fd54cdb0d0a
Thiago Tognoli <tt.dev@gmail.com>
2022-10-31 13:33:19 -0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,9 @@ import 'dotenv/config';
 export { createBot } from './createBot';
 
 export type { Bot, Message } from './createBot.types';
+
+export type { SendMessageResult } from './sendRequestHelper';
+export type { SendStatusResult } from './sendStatusHelper';
+
+export * from './messages.types';
+export * from './status.types';
